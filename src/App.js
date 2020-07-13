902c32386f18c50a203e8bb21fd5d5d900ed6734
jpmchargue <jpmchargue@gmail.com>
2020-07-13 15:48:06 -0500

    Created skeleton for login overlay.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <div class="header">
-          <p> Artiscribe </p>
-        </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overlayIsVisible: false,
+    }
+  }
 
-      </div>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/">
-          <Discover />
-        </Route>
-      </Switch>
-    </Router>
+  showOverlay() {
+    this.setState({overlayIsVisible: true})
+  }
+
+  hideOverlay() {
+    this.setState({overlayIsVisible: false})
+  }
+
+  renderOverlay() {
+    if (this.state.overlayIsVisible) return (<Overlay hideOverlay={() => this.hideOverlay()}/>);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div id="page">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/subs">
+            <Header />
+            <Subscriptions />
+            {this.renderOverlay()}
+          </Route>
+          <Route path="/">
+            <Header />
+            <Discover />
+            <ToggleButton onClick={() => this.showOverlay()} />
+            {this.renderOverlay()}
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function Header() {
+  return (
+    <div class="header">
+      <p> Artiscribe </p>
+    </div>
+  );
+}
+
+function ToggleButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Toggle Overlay
+    </button>
   );
 }
 
 function Discover() {
-  return <h1>Welcome to the discover page.</h1>
+  return (
+    <div id="main">
+      <h1>Welcome to the discover page.</h1>
+    </div>
+  );
 }
 
 function Login() {
@@ -41,6 +85,21 @@ function Login() {
 
 function Signup() {
   return <h1>Sign up</h1>
+}
+
+function Subscriptions() {
+  return <h1>Your Subscriptions</h1>
+}
+
+function Overlay(props) {
+  return (
+    <div id="overlay-shadow">
+      <div id="overlay">
+        Hello there
+        <button onClick={props.hideOverlay}>Hide Overlay</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
