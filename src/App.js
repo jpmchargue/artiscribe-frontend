e1774bd7560bebcc9d8d39ad066503e05c0d753a
jpmchargue <jpmchargue@gmail.com>
2020-07-13 14:25:34 -0500

    Added extremely basic routing.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div class="header">
+          <p> Artiscribe </p>
+        </div>
+
+      </div>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Discover />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function Discover() {
+  return <h1>Welcome to the discover page.</h1>
+}
+
+function Login() {
+  return <h1>Log in</h1>
+}
+
+function Signup() {
+  return <h1>Sign up</h1>
 }
 
 export default App;
